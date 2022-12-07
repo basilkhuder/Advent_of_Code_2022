fa29a75521f47142e372664d759204e94e73bd32
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2022-12-06 23:07:28 -0700

    Create day2.R

---
+++ day2.R
@@ -0,0 +1,38 @@
+# Advent of Code 2022 -----------------------------------------------------
+
+# Part One ----------------------------------------------------------------
+
+input <- read.table("input2.txt")
+combinations <- c("CZ" = 3,"BY" = 3,"AX" = 3, 
+                  "AZ" = 0, "CY" = 0,"BX" = 0,
+                  "BZ" = 6, "AY" = 6, "CX" = 6)
+scores <- c("X" = 1, "Y" = 2,"Z" = 3)
+
+input$combo <- paste0(input$V1, input$V2)
+input$hand_score <- scores[input$V2]
+input$outcome <- combinations[input$combo]
+
+sum(input$outcome + input$hand_score)
+
+
+# Part Two ----------------------------------------------------------------
+
+outcome <- \(type, letter) {
+  
+  if (type == "X") combo <- switch(letter, C = "CY", B = "BX", A = "AZ")
+  if (type == "Y") combo <- switch(letter, C = "CZ", B = "BY", A = "AX")
+  if (type == "Z") combo <- switch(letter, C = "CX", B = "BZ", A = "AY")
+  
+  combo
+  
+}
+
+input$combo <- sapply(seq(nrow(input)), \(x) outcome(input$V2[x], input$V1[x]))
+input$V2 <- sapply(strsplit(input$combo, ""), \(x) x[[2]])
+input$hand_score <- scores[input$V2]
+input$outcome <- combinations[input$combo]
+
+sum(input$outcome + input$hand_score)
+
+
+
